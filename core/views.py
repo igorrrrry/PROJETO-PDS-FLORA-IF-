281512164ec671e503752f4f400843ca98f76e0b
Brotinhooos <brotinhootaku@gmail.com>
2026-09-16 17:19:42 -0300

    templatedos

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'home.html')
 def arvore(request):
     return render(request, 'arvore.html')
 def mapa(request):
