483a538dfe49d360db7f9e7c4cec6e7e194988da
Ana Clara Queiroz Bezerra <20231094010044@ifrn.local>
2026-09-18 14:16:51 -0300

    sla

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('',views.home, name='home'),
     path('arvore/',views.arvore, name='arvore'),
     path('mapa/', views.mapa, name='mapa'),
+    path('curiosidades/', views.curiosidades, name='curiosidades'),
+    path('especie/', views.especie, name='especie'),
+    path('especiecatalogos/', views.especiecatalogos, name='especiecatalogos'),
+    path('sobre/', views.sobre, name='sobre'),
+    
     ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'core/home.html')
 def arvore(request):
     return render(request, 'arvore.html')
 def mapa(request):
     return render(request, 'mapa.html')
+def curiosidades(request):
+    return render(request, 'curiosidades.html')
+def especie(request):
+    return render(request, 'especie.html')
+def especiecatalogos(request):
+    return render(request, 'especiescatalogos.html')
+def sobre(request):
+    return render (request, 'sobre.html')
